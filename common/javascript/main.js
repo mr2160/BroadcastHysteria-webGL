47f751214f6c9d3e99df17cc7f6d66e2cbf70dcc
justmarolt <nik.marolt09@gmail.com>
2021-11-22 19:44:37 +0100

    Opozorila skačejo na 5 sekund

--- common/javascript/main.js
+++ common/javascript/main.js
@@ -1,7 +1,44 @@
 
-function buttonclick() {
+const modal = document.getElementById("modal");
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
-    window.location.href = "../../game/game.html"
+const interval = setInterval(function() {
+    openModal(modal);
+    // method to be executed;
+}, 5000);
+
+const interval2 = setInterval(function() {
+    closeModal(modal);
+    // method to be executed;
+}, 10000);
 
 
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
 }
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+//clearInterval(interval);
+//clearInterval(interval2);
